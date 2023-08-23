74bca13ab5e477f1bafc01a6ba18b966c571c5b0
Mohit <mohit.badola.20@gmail.com>
2023-08-23 16:32:17 +0530

    employee-manager

---
+++ Frontend/src/components/FooterComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+      <footer className='footer'>
+        <span className='text'>All Rights Reserved 2023 @JA50N</span>
+      </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
